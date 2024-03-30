89605423c9808bfd8955f8328bb2e1a2c0ac2077
Kirill Semibratov <KSemibratoFF@gmail.com>
2024-03-30 15:39:31 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+        while(N > 0)
+    {
+        int currentDigit = N % 10;
+        N = N / 10;
+        
+        if(N > 0)
+        {
+            Console.Write(currentDigit +",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit); 
+        }
+        
+    }  
+
